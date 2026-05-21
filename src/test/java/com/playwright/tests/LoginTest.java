a6cdbff8a879f3e97825483a2b2dd2bb35631994
YenumulaManikanta <manikantayenumula834@gmail.com>
2026-05-21 17:12:44 +0530

    Updated LoginTest.java

--- src/test/java/com/playwright/tests/LoginTest.java
+++ src/test/java/com/playwright/tests/LoginTest.java
@@ -8,8 +8,7 @@ public class LoginTest extends BaseTest{
 	
 	@Test
 	public void loginTest() {
-		page.navigate("https://example.com");
-		page.waitForTimeout(3000);
+		page.navigate("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 	    System.out.println("Login Test: " + Thread.currentThread().getId());
 
 	}
